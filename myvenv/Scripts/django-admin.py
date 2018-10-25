f26cdad4b58a0be4bbfdf320222188e2476f9a69
Cristian <alu0100891843@ull.edu.es>
2018-10-25 13:49:52 +0100

    Django start project

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\crist\google drive\innova7\tutorial 2\djangogirls_tutorial_cristian\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
