23d217907c1f90d2ccfd8f7825f8f3a216cd1aa8
Cristian <alu0100891843@ull.edu.es>
2018-10-26 12:47:11 +0100

    Dynamic data in templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {}) #La vista retorna este html
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #Hacemos el QuerySet
+    return render(request, 'blog/post_list.html', {'posts': posts}) #La vista retorna este html, además del QuerySet
+    #request es todo lo que recibimos del usuario desde internet
+    #posts lo que hará será mostrar los datos según las restricciones que tenga el queryset
