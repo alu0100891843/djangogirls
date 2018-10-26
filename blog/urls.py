2e3835609da6f9205ea79104299995f4a984d42e
Cristian <alu0100891843@ull.edu.es>
2018-10-26 12:10:24 +0100

    Deploying post PythonAnywhere

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'), #Asignamos que a la dirección / mostrará views.post_list
+    path('', views.post_list, name='post_list'), #Asignamos que a la dirección / mostrará la vista que está en views.post_list
 ]
